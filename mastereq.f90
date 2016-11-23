b1e29649167bd8200aee35983986b75689150170
Oliver Thomas <ot561@york.ac.uk>
2016-11-23 11:37:52 +0000

    successfully created tensor product function. only work with square matricies currently, should not be difficult to generalise. need to check i'm using column major ordering as fortran is quicker column major compared to row major.

--- mastereq.f90
+++ mastereq.f90
@@ -7,10 +7,10 @@ integer, parameter :: dp1=selected_real_kind(15,300)
 integer :: i, n_b, n_a
 
 !Matrix operators
-real(kind=dp1), allocatable, dimension (:,:) :: creation, annihilation, nummatrix, sigmaz, rho
+real(kind=dp1), allocatable, dimension (:,:) :: creation, annihilation, nummatrix, sigmaz, rho, large
 
 !number of states bosonic field
-n_b=100
+n_b=2
 
 !number of states atom
 n_a=2
@@ -20,14 +20,54 @@ call makeoperators
 
 !- Print operators to terminal (testing only)
 !call checkm
-
-
-
-
-
-
+do i=1, size(creation,1)
+print*, creation(i,:)
+end do
+print*, 
+do i=1, size(annihilation,1)
+print*, annihilation(i,:)
+end do
+large= tproduct(creation,annihilation)
+do i=1, size(large,1)
+print*, large(i,:)
+end do
 
 contains
+
+function tproduct(a,b)
+
+real(kind=dp1), dimension (:,:), intent(in) :: a, b
+real(kind=dp1), allocatable, dimension(:,:) :: tproduct
+real(kind=dp1), allocatable, dimension(:,:) :: tprod
+integer :: ierr, sindex1, sindex2, n, i,j,k,l, c_col, c_row
+
+sindex1=size(a, 1)*size(b, 1)
+sindex2=size(a, 2)*size(b, 2)
+
+allocate(tprod(sindex1, sindex2), stat=ierr)
+  if (ierr/=0) stop 'Error in allocating tproduct'
+
+n=size(a,1)
+c_col=0
+c_row=0
+print *, n
+print*, ' c_col ', ' c_row ', ' k ', ' l ', ' c_col+k ', ' c_row+l '
+do i=1, n
+  do j=1, n
+    do k=1, n
+      do l=1, n
+        tprod(c_col+j,c_row+l) = a(i,j)*b(k,l)
+	print*, c_col, c_row, j, l, c_col+j, c_row+l, tprod(c_col+j,c_row+l)
+      end do
+      c_row= c_row+n
+    end do
+    c_row=0
+  end do
+  c_col=c_col+n
+end do
+tproduct=tprod
+end function tproduct
+
 !-------------- Rhodot ---------------------------
 function f1(t, rho) 
   real (kind=dp1), dimension(3) :: p, v, f1, rho, rhodot
