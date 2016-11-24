ae49969b5b6eba86ea12c1af4008ff13bef13d52
Oliver Thomas <ot561@york.ac.uk>
2016-11-24 16:34:50 +0000

    added in basis states for atom, tensor product function only work for rank-2 arrays, unsure how to generalise but can harcode in sigma + & sigma- operators. created identity matrix function which works. continues on hamiltonian going to try Tensor producting operators by the other system's identity matrix which should work. made commutator function which needs further checking by hand but I think is ok. had to change array allocation in f1 (rhodot) and RK4 method to use the size of the density matrix in. using test arrays to check functions and subroutines are giving correct answers. need to finish hamiltonian and lindblad super op still.

--- mastereq.f90
+++ mastereq.f90
@@ -7,39 +7,82 @@ integer, parameter :: dp1=selected_real_kind(15,300)
 integer :: i, n_b, n_a
 
 !Matrix operators
-real(kind=dp1), allocatable, dimension (:,:) :: creation, annihilation, nummatrix, sigmaz, rho, large
+real(kind=dp1), allocatable, dimension (:,:) :: creation, annihilation, nummatrix, sigmaz, sigminus, sigplus
+real(kind=dp1), allocatable, dimension (:,:) :: rho, large, aident, bident, ham, test,test2
 
+
+real(kind=dp1), dimension (2) :: aup, adown, atup, atdown
 !number of states bosonic field
 n_b=4
 
 !number of states atom
 n_a=2
 
+aup = [1,0]
+adown= [0,1]
+print*, aup(:), adown(:)
+print*,
+
 !-Make operator matrices
-call makeoperators
+ call makeoperators
 
 !- Print operators to terminal (testing only)
-call checkm
+ !call checkm
 
 large= tproduct(nummatrix,sigmaz)
 do i=1, size(large,1)
-print*, large(i,:)
+!print*, large(i,:)
+end do
+
+!check identity matrices generated correctly
+print*,
+aident=identity(n_a)
+do i=1, size(aident,1)
+print*, aident(i,:)
+end do
+bident=identity(n_b)
+
+!checking tensor product of nummber matrix and a_ident gives correct shape.
+ham = hamiltonian(n_a, n_b, creation, annihilation, adown, aup)
+do i=1, size(ham,1)
+print*, ham(i,:)
+end do
+print *,
+
+!check commutator function is working correctly
+test=commutator(creation,annihilation,0)
+test2 = matmul(creation,annihilation)
+test2=test2 -matmul(annihilation,creation)
+do i=1, size(test,1)
+print*, test(i,:), test2(i,:)
 end do
 
 ! --------------- End of main program --------------------------------!
 contains
 
+!------------------------------ Identity -----------------------------------
+function identity(n)
+real(kind=dp1), dimension(n,n) :: identity
+integer :: n, aloerr, i
+identity=0
+do i=1,n
+  identity(i,i)=1
+end do
+end function identity
+
 !-------------- Rhodot ---------------------------
 function f1(t, rho) 
-  real (kind=dp1), dimension(:,:) :: v, f1, rho, rhodot
+  real (kind=dp1), dimension(:,:) :: rho
+  real(kind=dp1), dimension(size(rho,1),size(rho,2)) :: f1
   real (kind=dp1) :: t, constant=1, gamma=1
-  rhodot= constant*commutator(hamiltonian(1),rho,anti=0) + gamma*lindblad(a,adag,rho,anti=1)
-  f1=rhodot
+  !f1= constant*commutator(hamiltonian,rho, 0) + gamma*lindblad(a,adag,rho,1)
 end function f1
 !------------------------------------------------------
 !---------------------- Commutator ------------------------------------------
 function commutator(a,b,anti)
-  real(kind=dp1), dimension(:,:) :: a,b, commutator
+  real(kind=dp1), dimension(:,:) :: a,b
+real(kind=dp1), dimension(size(a,1),size(b,2)) :: commutator
+  integer :: anti
   if (anti==1) then
     commutator = matmul(a,b) + matmul(b,a)
   else 
@@ -49,11 +92,16 @@ end function commutator
 !--------------------------------------------------------------------------
 
 !----------------------- Hamiltonian -----------------------------------
-function hamiltonian(g)
-real(kind=dp1), dimension(:,:) :: hamiltonian
-real(kind=dp1) :: g
-hamiltonian = 1
-end function hamiltonaian
+function hamiltonian(n_a,n_b,creat,anni,adown,aup)
+real(kind=dp1), dimension(:,:), allocatable :: a_i, b_i, creat, anni
+real(kind=dp1), dimension (:) :: adown, aup
+integer :: n_a, n_b
+real(kind=dp1), dimension(n_a*n_b,n_a*n_b) :: hamiltonian
+real(kind=dp1) :: g=1, omega_b=1, omega_a=1
+a_i=identity(n_a)
+b_i=identity(n_b)
+hamiltonian = omega_b*tproduct(nummatrix,a_i)
+end function hamiltonian
 
 !---------------------- Tensor/ Outer Product function --------------------
 function tproduct(a,b)
@@ -99,8 +147,8 @@ end function tproduct
 !Runge-kutta Sub
 !f1 is rhodot
 subroutine rk4(h,t,rho)
-  
-    real(kind=dp1), dimension(3) :: k_1, k_2, k_3, k_4, rho
+    real(kind=dp1), dimension(:,:) :: rho
+    real(kind=dp1), dimension(size(rho,1),size(rho,2)) :: k_1, k_2, k_3, k_4
     real(kind=dp1) :: t, h
 
     k_1 =h*f1(t, rho)
