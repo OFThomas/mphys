d77e9169ef23416effff56c6711e5df3a29aeaba
Oliver Thomas <ot561@york.ac.uk>
2017-02-02 14:58:26 +0000

    sorted data into folders. added hermitain check for rho and percentage printing out. cleaned up code, changed to intent(in). ran for nb20 and nb60 g=0.1,0.2. need to check convergence of <> for 20 to 50 nb in 3 different intial states

--- mastereq.f90
+++ mastereq.f90
@@ -1,30 +1,43 @@
 !Oliver Thomas 2016
-
 program mastereq
 
 use matrixfns
 implicit none
 
-real(kind=dp1) :: timestep, total_time
+real(kind=dp1) :: timestep, total_time, timeout, couplingstrength
 complex(kind=dp1), allocatable, dimension (:,:) :: h
 complex(kind=dp1), allocatable, dimension (:) :: heigen
 complex(kind=dp1), dimension (1,1) :: dummy
 complex(kind=dp1), allocatable, dimension(:) :: work
-integer(kind=dp1) :: ok, worksize
+integer :: ok,worksize,outerloop,innerloop
+integer :: reducedtime,state,findh
+
+!---------------------------- INPUTS----------------------------------------
+!Simulated time
+total_time=30_dp1
+!Time steps
+timestep=2*1e-2_dp1
 
 !number of states bosonic field
 n_b=60
-
 !number of states atom
 n_a=2
 
-!Simulated time
-total_time=1*1e2_dp1
-!Time steps
-timestep=5*1e-2_dp1
+!initial starting state,0=vacuum, 1=mixed, 2=max_excitations
+state=0
+
+!define g
+couplingstrength=0.2_dp1
+
+!find H eigenspectrum, 0=no,1=yes 
+findh=0
+!-------------------------- END OF INPUTS ----------------------------------
+!how many steps to integrate
 timesteps=nint(total_time/timestep)
+!show user simulation parameters
 print*, 'Time simulated 		', 'Timestep 		', 'Total steps '
 print*, total_time, timestep, timesteps
+print*, n_b, 'Photon states', n_a, 'Atom states'
 
 !Matrix operators
 
@@ -33,89 +46,113 @@ print*, total_time, timestep, timesteps
 
 !-Make operator matrices
  call makeoperators
+  hamil=hamiltonian(n_a, n_b, creation, annihilation, sigmaz, sigmaminus, sigmaplus, couplingstrength)
 
 rhob = 0
 rhoa = 0
-!----------------------- Vacuum, ground state ---------------------------------!
-!!initialising density matrix into vacuum photon
-rhob(1,1,1)=1
-!!g.s. atom
-rhoa(2,2,1)=1
-
-!----------------------- Highest excited state --------------------------------!
-!rhob(n_b,n_b,1)=1
-!rhoa(1,1,1)=1
-!-------------------------------------------------------------------------------
-!----------------------- Mixed state --------|0><0| x (|g><g| + |e><e|)/2 ------
-!rhob(1,1,1)=1
-!rhoa(1,1,1)=0.5_dp1
-!rhoa(2,2,1)=0.5_dp1
-
-
+!specifying initial state
+if (state==0) then
+    !-------------- Vacuum, ground state ---------------------------------!
+    !!initialising density matrix into vacuum photon
+    rhob(1,1,1)=1
+    !!g.s. atom
+    rhoa(2,2,1)=1
+else if (state==1) then
+    !------------- Mixed state ---|0><0| x (|g><g| + |e><e|)/2 ------
+    rhob(1,1,1)=1
+    rhoa(1,1,1)=0.5_dp1
+    rhoa(2,2,1)=0.5_dp1
+else if (state==2) then
+    !-------------------- Highest excited state -------------------------!
+    rhob(n_b,n_b,1)=1
+    rhoa(1,1,1)=1
+else 
+    print*, 'please enter a valid state'
+end if 
 !total density matrix = tproduct 
 rho(:,:,1)=tproduct(rhob(:,:,1),rhoa(:,:,1))
 
  call openoutputfiles
 
+!initialising and splitting into intervals
 t=0
+reducedtime=nint(timesteps/10.0_dp1)
+
 !Increment rho using runge-kutta, save results in rho array
-main: do counter=1,timesteps-1
-  rho(:,:,counter+1)=rk4(timestep,t,rho(:,:,counter))
-  rho(:,:,counter+1) = rho(:,:,counter+1)/trace(rho(:,:,counter+1))
-  if (maxval(abs(rho(:,:,counter+1)-rho(:,:,counter)))<=1e-5) then
-    print*,'steady state exit'
-    timesteps=counter+1
-    exit main
+main:do outerloop=1,10
+  do innerloop=1,reducedtime
+    counter=innerloop + reducedtime*(outerloop-1)
+    rho(:,:,counter+1)=rk4(timestep,t,rho(:,:,counter))
+    !normalising
+    rho(:,:,counter+1) = rho(:,:,counter+1)/trace(rho(:,:,counter+1))
+  end do 
+  !Steady state check
+  if (maxval(abs(rho(:,:,counter+1)-rho(:,:,counter)))<=1e-8) then
+      print*,'steady state exit'
+      timesteps=counter+1
+      exit main
   end if
+  !Hermitian check
+  if (maxval(abs(rho(:,:,timesteps) - transpose(conjg(rho(:,:,timesteps)))))>=1e-15) then 
+   print*,'Rho is not Hermititan EXIT'
+      timesteps=counter+1
+      exit main
+  end if
+print*, outerloop*10, '% done'
 end do main
 
-!Initial
+!Initial values
 write(11,*) real(rho(:,:,1),kind=dp1)
-!Write out values
+!Write out final values
 write(11,*) real(rho(:,:,timesteps),kind=dp1)
 
+!Expectation values
 do i=1, timesteps
-  write(13,*) trace(rho(:,:,i))
-  write(14,*) trace(matmul(creation(:,:),matmul(annihilation(:,:),rho(:,:,i))))
-  write(15,*) trace(matmul(rho(:,:,i),sigmaz(:,:)))
-  write(16,*) trace(matmul(rho(:,:,i),sigmax(:,:)))
-  write(17,*) trace(matmul(rho(:,:,i),sigmay(:,:)))
+  timeout=(i-1)*timestep
+  write(13,*) timeout, trace(rho(:,:,i))
+  write(14,*) timeout, trace(matmul(creation(:,:),matmul(annihilation(:,:),rho(:,:,i))))
+  write(15,*) timeout, trace(matmul(rho(:,:,i),sigmaz(:,:)))
+  write(16,*) timeout, trace(matmul(rho(:,:,i),sigmax(:,:)))
+  write(17,*) timeout, trace(matmul(rho(:,:,i),sigmay(:,:)))
 end do
 
-!!!! ----------------- Testing eigenspectrum ----------------------
+if (findh==1) then
+  !!!! ----------------- Testing eigenspectrum ----------------------
+  !allocate eigenvalue matrix
+  allocate(heigen(n_a*n_b))
+  !allocate work temporary matrix for zgeev subroutine
+  worksize=(int(4.0_dp1*(n_a*n_b)))
+  allocate(work(worksize))
 
-!allocate eigenvalue matrix
-allocate(heigen(n_a*n_b))
-!initialise matrices
-heigen =0
-dummy=0
-!allocate work temporary matrix for zgeev subroutine
-worksize=(int(4.0_dp1*(n_a*n_b)))
+  !initialise matrices
+  heigen =0
+  dummy=0
+  work=0
+  coupl=0.0_dp1
 
-allocate(work(worksize))
-work=0
-coupl=0.0_dp1
+  !go incrementing the coupling strength calculate the eigenspectrum
+  do j=0,1
+    h=hamiltonian(n_a,n_b,creation,annihilation,sigmaz,sigmaminus,sigmaplus,coupl)
 
-!go incrementing the coupling strength calculate the eigen spectra
-do j=0,1
-  h=hamiltonian(n_a,n_b,creation,annihilation,sigmaz,sigmaminus,sigmaplus,coupl)
-
-!!using lapack zgeev subroutine for complex matrix eigenvalues
- call zgeev('N','N', size(h,1), h, size(h,1), heigen, dummy, 1, dummy, 1, work, worksize, work, ok)
-
-if (ok .eq. 0) then
-    write(20,*) coupl,real(heigen(:),kind=dp1)
-else
-  print*, 'Error with zgeev'
-end if
-
+    !!using lapack zgeev subroutine for complex matrix eigenvalues
+    call zgeev('N','N', size(h,1), h, size(h,1), heigen, dummy, 1, dummy, 1, work, worksize, work, ok)
+    !check to see if zeegev exited with errors
+    if (ok .eq. 0) then
+      write(20,*) coupl,real(heigen(:),kind=dp1)
+    else
+      print*, 'Error with zgeev'
+    end if
   coupl=coupl+0.02_dp1
 end do
-
+else
+  print*,'not finding Hamiltonian eigenspectrum'
+end if 
 !-----------------------------------------------------------------------
+!Writing out final hermitian check t=end
 write(12,*) rho(:,:,timesteps) - transpose(conjg(rho(:,:,timesteps)))
 print*,
-print*, maxval(real(rho(:,:,timesteps) - transpose(conjg(rho(:,:,timesteps))),kind=dp1))
+!Print max value of hermitian check and trace to terminal to check validity of run
+print*, maxval(abs(rho(:,:,timesteps) - transpose(conjg(rho(:,:,timesteps)))))
 print*, trace(rho(:,:,timesteps))
 
 
--- matrixfns.f90
+++ matrixfns.f90
@@ -1,5 +1,4 @@
 !Oliver Thomas 2016
-
 module matrixfns
 implicit none
 
@@ -8,56 +7,63 @@ integer :: i, n_b, n_a, counter, status ,timesteps, j
 complex(kind=dp1), allocatable, dimension (:,:) :: creation, annihilation, nummatrix
 complex(kind=dp1), allocatable, dimension (:,:) :: sigmaz, sigmaminus, sigmaplus
 complex(kind=dp1), allocatable, dimension (:,:) :: sigmax, sigmay
-complex(kind=dp1), allocatable, dimension (:,:) :: aident, bident, hamil!, h, heigen
+complex(kind=dp1), allocatable, dimension (:,:) :: aident, bident, hamil
 complex(kind=dp1), allocatable, dimension (:,:,:) :: rho, rhoa, rhob
 real(kind=dp1) :: t, coupl
 complex(kind=dp1) :: imaginary=(0.0_dp1,1.0_dp1)
 
 contains
-
-!-------------------Trace -------------------------------------------------
+!-------------------Trace function -----------------------------------------
 function trace(a)
-real(kind=dp1) :: trace
-complex(kind=dp1), dimension(:,:) :: a
-integer :: i
-trace=0.0_dp1
-if (size(a,1)==size(a,2)) then
-  do i=1, size(a,1)
-    trace=trace+a(i,i)
-  end do
-else 
-  print*, 'Error not a square matrix!'
-end if
+  real(kind=dp1) :: trace
+  complex(kind=dp1), dimension(:,:), intent(in) :: a
+  integer :: i
+  
+  trace=0.0_dp1
+!square matrix check
+  if (size(a,1)==size(a,2)) then
+    do i=1, size(a,1)
+      trace=trace+a(i,i)
+    end do
+  else 
+    print*, 'Error not a square matrix!'
+  end if
 end function trace
-!--------------------------- End of Trace -----------------------------------
-!------------------------------ Identity -----------------------------------
+!--------------------------- End of Trace----------------------------------
+!------------------------------ Identity function--------------------------
 function identity(n)
-real(kind=dp1), dimension(n,n) :: identity
-integer :: n, aloerr, i
-identity=0
-do i=1,n
-  identity(i,i)=1
-end do
+  real(kind=dp1), dimension(n,n) :: identity
+  integer, intent(in) :: n
+  integer :: aloerr, i
+  
+  identity=0
+  do i=1,n
+    identity(i,i)=1
+  end do
 end function identity
 !-----------------------------End of Identity-------------------------------
 
 !-------------------------------- Rhodot -----------------------------------
 function f1(t, rho) 
-  complex(kind=dp1), dimension(:,:) :: rho
+  complex(kind=dp1), dimension(:,:), intent(in) :: rho
   complex(kind=dp1), dimension(size(rho,1),size(rho,2)) :: f1
-  real (kind=dp1) :: t, gamma=0.1_dp1
+  real (kind=dp1), intent(in) :: t
+  real(kind=dp1) :: gamma=0.1_dp1
   complex(kind=dp1) :: constant, imaginary=(0.0_dp1,1.0_dp1)
-  constant=-imaginary
-!as H time indep can generate once in make operators and use the matrix to save time.
+  
+constant=-imaginary
+!as H time indep can generate once in make operators 
+!and use the matrix to save time.
   f1= constant*commutator(hamil,rho, 0) + gamma*lindblad(n_a,n_b,annihilation,creation,rho,1)
 end function f1
 !-----------------------------End of Rhodot---------------------------------
 
 !---------------------- Commutator --------------------------------------
 function commutator(a,b,anti)
-  complex(kind=dp1), dimension(:,:) :: a,b
- complex(kind=dp1), dimension(size(a,1),size(b,2)) :: commutator
-  integer :: anti
+  complex(kind=dp1), dimension(:,:), intent(in) :: a,b
+  complex(kind=dp1), dimension(size(a,1),size(b,2)) :: commutator
+  integer, intent(in) :: anti
+
 !anti for lindlbad
   if (anti==1) then
     commutator = matmul(a,b) + matmul(b,a)
@@ -70,15 +76,16 @@ end function commutator
 
 !----------------------- Hamiltonian -----------------------------------
 function hamiltonian(n_a,n_b,creat,anni, sigz, sigm, sigp, g)
-complex(kind=dp1), dimension(:,:), allocatable :: a_i, b_i, creat, anni, sigz, sigp, sigm
-integer :: n_a, n_b
-complex(kind=dp1), dimension(n_a*n_b,n_a*n_b) :: hamiltonian, wcoupling, scoupling
-real(kind=dp1) :: g, omega_b=1.0_dp1, omega_a=1.0_dp1
+  complex(kind=dp1), dimension(:,:), allocatable, intent(in) :: creat, anni, sigz, sigp, sigm
+  integer, intent(in) :: n_a, n_b
+  complex(kind=dp1), dimension(n_a*n_b,n_a*n_b) :: hamiltonian, wcoupling, scoupling
+  real(kind=dp1), intent(in) :: g
+  real(kind=dp1) :: omega_b=1.0_dp1, omega_a=1.0_dp1
 
-wcoupling= matmul(creat,sigm) +matmul(sigp,anni)
-scoupling = matmul(creat,sigp) + matmul(sigm,anni)
+  wcoupling= matmul(creat,sigm) +matmul(sigp,anni)
+  scoupling = matmul(creat,sigp) + matmul(sigm,anni)
 
-hamiltonian = omega_b*matmul(creat,anni)+0.5_dp1*omega_a*sigz + g*(wcoupling+scoupling)
+  hamiltonian = omega_b*matmul(creat,anni)+0.5_dp1*omega_a*sigz + g*(wcoupling+scoupling)
 
 end function hamiltonian
 !----------------------------End of Hamiltonian---------------------------
@@ -86,11 +93,12 @@ end function hamiltonian
 !------------------------- Lindblad super operator--------------------------
 
 function lindblad(n_a,n_b,a,adag,rho,comm)
-complex(kind=dp1), dimension(:,:) :: a, adag, rho
-integer :: n_a, n_b, comm
-complex(kind=dp1), dimension(n_a*n_b,n_a*n_b) :: lindblad
+  complex(kind=dp1), dimension(:,:), intent(in) :: a, adag, rho
+  integer, intent(in) :: n_a, n_b, comm
+  complex(kind=dp1), dimension(n_a*n_b,n_a*n_b) :: lindblad
+
 !comm=1 for anti commutation
-lindblad = 2.0_dp1*matmul(a,matmul(rho,adag)) - commutator(matmul(adag,a),rho,1)
+  lindblad = 2.0_dp1*matmul(a,matmul(rho,adag)) - commutator(matmul(adag,a),rho,1)
 end function lindblad
 
 !---------------------------End of super operator-------------------------
@@ -98,161 +106,139 @@ end function lindblad
 !---------------------- Tensor Product function --------------------
 function tproduct(a,b)
 
-complex(kind=dp1), dimension (:,:) :: a, b
-complex(kind=dp1), allocatable, dimension(:,:) :: tproduct
-complex(kind=dp1), allocatable, dimension(:,:) :: tprod
-integer :: ierr, sindex1, sindex2, i,j,k,l, n_a1, n_a2, n_b1, n_b2
+  complex(kind=dp1), dimension (:,:), intent(in) :: a, b
+  complex(kind=dp1), allocatable, dimension(:,:) :: tproduct
+  complex(kind=dp1), allocatable, dimension(:,:) :: tprod
+  integer :: ierr, sindex1, sindex2, i,j,k,l, n_a1, n_a2, n_b1, n_b2
 
-sindex1=size(a, 1)*size(b, 1)
-sindex2=size(a, 2)*size(b, 2)
+  sindex1=size(a, 1)*size(b, 1)
+  sindex2=size(a, 2)*size(b, 2)
+  allocate(tprod(sindex1, sindex2), stat=ierr)
+    if (ierr/=0) stop 'Error in allocating tproduct'
+  n_a1=size(a,1)
+  n_a2=size(a,2)
+  n_b1=size(b,1)
+  n_b2=size(b,2)
 
-allocate(tprod(sindex1, sindex2), stat=ierr)
-  if (ierr/=0) stop 'Error in allocating tproduct'
+  do j=1, n_a2
+    do i=1, n_a1
+      do l=1, n_b2
+        do k=1, n_b1
+          tprod(k+(i-1)*n_b1, l+(j-1)*n_b2) = a(i,j)*b(k,l)
+        end do !k
+      end do !l
+    end do !i
+  end do !j
 
-n_a1=size(a,1)
-n_a2=size(a,2)
-n_b1=size(b,1)
-n_b2=size(b,2)
-
-do j=1, n_a2
-  do i=1, n_a1
-    do l=1, n_b2
-      do k=1, n_b1
-        tprod(k+(i-1)*n_b1, l+(j-1)*n_b2) = a(i,j)*b(k,l)
-      end do !k
-    end do !l
-  end do !i
-end do !j
-
-tproduct=tprod
+  tproduct=tprod
 end function tproduct
 !--------------------------- End of Tensor Product -----------------------
 
 !---------------------------- R4K ---------------------------------
 !Runge-kutta !f1 is rhodot
 function rk4(h,t,rho)
-    complex(kind=dp1), dimension(:,:), intent(in) :: rho
-    complex(kind=dp1), dimension(size(rho,1),size(rho,2)) :: rk4, k_1, k_2, k_3, k_4
-    real(kind=dp1) :: t, h
+  complex(kind=dp1), dimension(:,:), intent(in) :: rho
+  complex(kind=dp1), dimension(size(rho,1),size(rho,2)) :: rk4, k_1, k_2, k_3, k_4
+  real(kind=dp1) :: t, h
 
-    k_1 =h*f1(t, rho)
-    k_2 = h*f1(t+0.5_dp1*h, rho + 0.5_dp1*k_1)
-    k_3 = h*f1(t+0.5_dp1*h, rho + 0.5_dp1*k_2)
-    K_4 = h*f1(t+h, rho + k_3)  
-    rk4 = rho + (k_1 + 2.0_dp1*k_2 + 2.0_dp1*k_3 + k_4)/6.0_dp1
+  k_1 =h*f1(t, rho)
+  k_2 = h*f1(t+0.5_dp1*h, rho + 0.5_dp1*k_1)
+  k_3 = h*f1(t+0.5_dp1*h, rho + 0.5_dp1*k_2)
+  K_4 = h*f1(t+h, rho + k_3)  
+  rk4 = rho + (k_1 + 2.0_dp1*k_2 + 2.0_dp1*k_3 + k_4)/6.0_dp1
     
-    t=t+h
+  t=t+h
 end function rk4
 !-----------------------End of R4K-----------------------------
 
 !----------------- Allocate matrix operators -------------------
 subroutine makeoperators
+  real(kind=dp1) :: root
+  integer :: aloerr
 
-real(kind=dp1) :: root
-integer :: aloerr
-
-allocate(creation(n_b,n_b), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating creationop'
-
-allocate(annihilation(n_b,n_b), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating annihilationop'
-
-allocate(sigmaz(n_a,n_a), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating sigmazop'
-
-allocate(sigmaplus(n_a,n_a), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating sigma+op'
-
-allocate(sigmaminus(n_a,n_a), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating sigma-op'
-
-allocate(rhoa(n_a,n_a, timesteps), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating rhoa'
-
-allocate(rhob(n_b,n_b, timesteps), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating rhob'
-
-allocate(rho(n_b*n_a,n_b*n_a, timesteps), stat=aloerr)
-if (aloerr/=0) stop 'Error in allocating rho'
-
+  allocate(creation(n_b,n_b), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating creationop'
+  allocate(annihilation(n_b,n_b), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating annihilationop'
+  allocate(sigmaz(n_a,n_a), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating sigmazop'
+  allocate(sigmaplus(n_a,n_a), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating sigma+op'
+  allocate(sigmaminus(n_a,n_a), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating sigma-op'
+  allocate(rhoa(n_a,n_a, timesteps), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating rhoa'
+  allocate(rhob(n_b,n_b, timesteps), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating rhob'
+  allocate(rho(n_b*n_a,n_b*n_a, timesteps), stat=aloerr)
+    if (aloerr/=0) stop 'Error in allocating rho'
 !--------------------- Populate-------------------
-creation=0
-annihilation=0
-do i=1, n_b-1
-  root=dsqrt(real(i,kind=dp1))
-  creation(i+1,i)=root
-  annihilation(i,i+1)=root
-end do
-nummatrix =matmul(creation, annihilation)
+  creation=0
+  annihilation=0
+  do i=1, n_b-1
+    root=dsqrt(real(i,kind=dp1))
+    creation(i+1,i)=root
+    annihilation(i,i+1)=root
+  end do
+  nummatrix =matmul(creation, annihilation)
 
-sigmaz=0
-sigmaz(1,1)= 1
-sigmaz(2,2)=-1
+  sigmaz=0
+  sigmaz(1,1)= 1
+  sigmaz(2,2)=-1
 
-sigmaplus=0
-sigmaplus(2,1)=1
-
-sigmaminus=0
-sigmaminus(1,2)=1
+  sigmaplus=0
+  sigmaplus(2,1)=1
+  sigmaminus=0
+  sigmaminus(1,2)=1
 
 !generate identities in respective spaces
- aident=identity(n_a)
- bident=identity(n_b)
+  aident=identity(n_a)
+  bident=identity(n_b)
 
 !make operators only act on their own system, use tensor product of identity in other space
- sigmaz=tproduct(bident,sigmaz)
- sigmaplus=tproduct(bident,sigmaplus)
- sigmaminus=tproduct(bident,sigmaminus)
+  sigmaz=tproduct(bident,sigmaz)
+  sigmaplus=tproduct(bident,sigmaplus)
+  sigmaminus=tproduct(bident,sigmaminus)
+  sigmax=sigmaminus+sigmaplus
+  sigmay=imaginary*(sigmaminus-sigmaplus)
 
- sigmax=sigmaminus+sigmaplus
- sigmay=imaginary*(sigmaminus-sigmaplus)
-
- nummatrix=tproduct(nummatrix,aident)
- creation=tproduct(creation, aident)
- annihilation=tproduct(annihilation,aident)
+  nummatrix=tproduct(nummatrix,aident)
+  creation=tproduct(creation, aident)
+  annihilation=tproduct(annihilation,aident)
 
 !calculate hamiltonian once 
- hamil=hamiltonian(n_a, n_b, creation, annihilation, sigmaz, sigmaminus, sigmaplus, 0.1_dp1)
+  !hamil=hamiltonian(n_a, n_b, creation, annihilation, sigmaz, sigmaminus, sigmaplus, 0.2_dp1)
 end subroutine makeoperators
 !------------------------- End of operators -----------------------
 
 subroutine openoutputfiles
-! To write the density matrix out
-open(unit=11, file='rho.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening rho output file'
-
-open(unit=12, file='rho1.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening rho output file'
-
-open(unit=13, file='rhotrace.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening rhotrace output file'
-
-open(unit=14, file='expectation_n.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening nexpectation output file'
-
-open(unit=15, file='expectation_sigz.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening nexpectation output file'
-
-open(unit=16, file='expectation_sigx.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening nexpectation output file'
-
-open(unit=17, file='expectation_sigy.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening nexpectation output file'
-
-open(unit=20, file='heigen.txt', status='replace', iostat=status)
-  if (status/=0) stop 'Error in opening heigen output file'
-
+!To write the density matrix out
+  open(unit=11, file='rho.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening rho output file'
+  open(unit=12, file='rho1.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening rho output file'
+  open(unit=13, file='rhotrace.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening rhotrace output file'
+  open(unit=14, file='expectation_n.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening nexpectation output file'
+  open(unit=15, file='expectation_sigz.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening nexpectation output file'
+  open(unit=16, file='expectation_sigx.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening nexpectation output file'
+  open(unit=17, file='expectation_sigy.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening nexpectation output file'
+  open(unit=20, file='heigen.txt', status='replace', iostat=status)
+    if (status/=0) stop 'Error in opening heigen output file'
 end subroutine openoutputfiles
 
 subroutine closeoutputfiles
-close(11)
-close(12)
-close(13)
-close(14)
-close(15)
-close(16)
-close(17)
-close(20)
+  close(11)
+  close(12)
+  close(13)
+  close(14)
+  close(15)
+  close(16)
+  close(17)
+  close(20)
 end subroutine closeoutputfiles
 end module matrixfns
-
