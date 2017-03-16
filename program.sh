1d60132f99e1e7fba93f74661f890627dc4fb373
Oliver Thomas <ot561@york.ac.uk>
2017-03-16 16:40:05 +0000

    ran script for nb=2 to 20. need to run for longer than t=80w. also need to create a file directory to clear folder up.

--- program.sh
+++ program.sh
@@ -1,16 +1,24 @@
 photonstates=10
 initialstate=0
-
+rm n.txt
+rm sigx.txt
+rm sigy.txt
+rm sigz.txt
 gfortran matrixfns.f90 mastereq.f90 -o mastereq.out -llapack -lblas
 
-time ./mastereq.out << EOF
-$photonstates
-$initialstate
+for photonstates in {2..20}
+do
+  time ./mastereq.out << EOF
+  $photonstates
+  $initialstate
 EOF
 
-for name in ./expectation_*.txt
-do
-  new=${name:14}     # remove path 	
-  cp $name ./$photonstates$initialstate$new
+  for name in ./expectation_*.txt
+  do
+    new=${name:14}     # remove path 	
+    cp $name ./$photonstates$initialstate$new
+    data=$(tail ./$photonstates$initialstate$new -n 1)
+    echo $photonstates $data >> ./$new
+  done
 done
 #./gplot.sh 
