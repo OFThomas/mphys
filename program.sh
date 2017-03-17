2fc09054d07547604091708f2cb5330616a17339
Oliver Thomas <ot561@york.ac.uk>
2017-03-17 12:11:09 +0000

    ran for nbupto 46 for vacuum state. going to run for max excited state to see if values converge back to vac & mixed.

--- program.sh
+++ program.sh
@@ -1,26 +1,29 @@
 photonstates=10
 initialstate=2
-rm n.txt
-rm sigx.txt
-rm sigy.txt
-rm sigz.txt
+#rm ($initialstate)n.txt
+#rm sigx.txt
+#rm sigy.txt
+#rm sigz.txt
 gfortran matrixfns.f90 mastereq.f90 -o mastereq.out -llapack -lblas
 
-time for photonstates in {2..20}
-do
-  time ./mastereq.out << EOF
-  $photonstates
-  $initialstate
+#time for initialstate in {0..2}
+#do
+  time for photonstates in {4..40..2}
+  do
+    time ./mastereq.out << EOF
+    $photonstates
+    $initialstate
 EOF
 
-  for name in ./expectation_*.txt
-  do
-    new=${name:14}     # remove path 	
-    cp $name ./expectation/$photonstates$initialstate$new
-    data=$(tail ./expectation/$photonstates$initialstate$new -n 1)
-    echo $photonstates $data >> ./expectation/$initialstate$new
+    for name in ./expectation_*.txt
+    do
+      new=${name:14}     # remove path 	
+      cp $name ./expectation/$photonstates$initialstate$new
+      data=$(tail ./expectation/$photonstates$initialstate$new -n 1)
+      echo $photonstates $data >> ./expectation/$initialstate$new
+    done
   done
-done
+#done
 #./gplot.sh 
 
 #cd ./expectation
